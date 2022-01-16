367a4bde2befc11ae7cf3b89567fc6414b9ae6cf
unknown <hindhujhasridhar@gmail.com>
2022-01-16 22:52:42 +0530

    ROOT,LEFT AND RIGHT NODE ARE CREATED

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,6 +6,10 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("BINARY SEARCH TREE PROGRAMS");
+            BinarySearchTree<int> search = new BinarySearchTree<int>(56);//root node 
+            search.Insert(30);//left node
+            search.Insert(70);//right node
+            search.Display();
         }
     }
 }
